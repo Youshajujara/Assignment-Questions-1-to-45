545d030b1c1f23f715fb20ffbc77f1959e29eba8
Youshajujara <64375736+Youshajujara@users.noreply.github.com>
2024-04-08 17:03:21 +0500

    Assignment Question 1 to 45

---
+++ app.ts
@@ -0,0 +1,63 @@
+// TypeScript Assignment 
+// Question No. 2
+
+// let a= "Yousha";
+// console.log("Hello "+a+" would you like to learn some Python today?");
+
+
+// Question No. 3
+
+// let a = "Yousha Yousuf";
+// let b = a.toUpperCase();
+// console.log(b);
+
+// let a = "Yousha Yousuf";
+// let b = a.toLowerCase();
+// console.log(b);
+
+// Question No. 4
+
+
+// console.log('Once Imran Khan said, "Absolutely not"')
+
+
+// Question No. 5
+
+// let quote: string = "Success is not final, failure is not fatal: It is the courage to continue that counts."
+// let author: string = "Winston Churchill";
+
+// console.log(`${author} Once said, "${quote}"`);
+
+// Question No. 6
+
+// let person = ' \n\tYousha Jujara'
+// console.log(person);
+
+// Question No. 7 and // Question No. 8
+
+// console.log(4+4);
+// console.log(8-0);
+// console.log(4*2);
+// console.log(16/2);
+
+// Question No. 9
+
+// let favNo = 1
+// let message = "Here is my favourite number "+ favNo;
+// console.log(message);
+
+// Question No. 10
+
+//Program 1 
+// Program written : Yousha
+// Current date : 23-03-2024
+// This program prints hello world
+// console.log("Hello World");
+
+// Program written : Yousha
+// Current date : 23-03-2024
+// This program prints the addition of two digit
+// console.log(2+2);
+
+// Question No. 11
+
